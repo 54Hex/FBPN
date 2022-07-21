268f1a18f385f5a725c4ac31ea5174c84c4b1d5d
wx <Azure.cwx@gmail.com>
2022-07-21 22:21:11 +0800

    fixed everything...?

--- FBPN/Main.swift
+++ FBPN/Main.swift
@@ -10,31 +10,42 @@ import Firebase
 import UserNotifications 
 
 struct Main: View {
-    @State private var usrInput: String = ""
+    @State var usrInput =  (String: "", title: "")
+    @State var placeHolder: String = "Write something here..."
     
     var body: some View {
         NavigationView {
             Form {
-                TextField("", text: $usrInput)
-                Button("Publish") {
-                    // action code here dumbass
+                Section(header: Text("Message")) {
+                    TextEditor(text: $usrInput.title)
+                       
+                    }
+                
+                Section {
+                    Button("Publish") {
+                        print("\(usrInput)")
+                    }
+                    .accentColor(Color(CC.Firery))
+                    
+                    
                 }
             }
+            
+            .navigationTitle("New Annoucement")
         }
-        .navigationTitle("Add annoucement")
-        
     }
 }
 
 
 
 struct CC {
-    static let DarkGray = "Gray"
-    static let firery = "Fire"
+    static let DarkGray = "DarkGray"
+    static let Firery = "Firery"
 }
 struct Main_Previews: PreviewProvider {
     static var previews: some View {
         Main()
             .preferredColorScheme(.dark)
+        
     }
 }
