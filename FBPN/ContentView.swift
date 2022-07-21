9a156284bf405b56afb76961947b8abe0927bbb2
wx <Azure.cwx@gmail.com>
2022-07-21 19:30:58 +0800

    Initial Commit

---
+++ FBPN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FBPN
+//
+//  Created by WX on 21/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FBPN/FBPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  FBPNApp.swift
+//  FBPN
+//
+//  Created by WX on 21/7/22.
+//
+
+import SwiftUI
+
+@main
+struct FBPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
